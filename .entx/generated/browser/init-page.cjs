1a68cb3836f6d7f1d2d08071384640601ef0efd9
Julio Cernadas <33937718+julio-cernadas@users.noreply.github.com>
2026-08-20 11:29:21 -0400

    compose changeset
    Live compose case: thread turn then changeset commit

--- .entx/generated/browser/init-page.cjs
+++ .entx/generated/browser/init-page.cjs
@@ -1,5 +1,5 @@
 module.exports = {
   default: async ({ page }) => {
-    await page.context().addCookies([{"name": "__session", "value": "API--VUwHFXJZauF7F-DTl1GV49FXgupvbz8KZwn2P4se63A", "url": "http://100.108.230.115:8000/", "httpOnly": true, "secure": false, "sameSite": "Lax"}, {"name": "__session", "value": "API--VUwHFXJZauF7F-DTl1GV49FXgupvbz8KZwn2P4se63A", "url": "http://127.0.0.1:3000/", "httpOnly": true, "secure": false, "sameSite": "Lax"}, {"name": "__session", "value": "API--VUwHFXJZauF7F-DTl1GV49FXgupvbz8KZwn2P4se63A", "url": "http://localhost:3000/", "httpOnly": true, "secure": false, "sameSite": "Lax"}]);
+    await page.context().addCookies([{"name": "__session", "value": "API--wN8IK3wqINdycqcAr2UWemb6lLIJWYCRROZMq-P7mik", "url": "http://100.108.230.115:8000/", "httpOnly": true, "secure": false, "sameSite": "Lax"}, {"name": "__session", "value": "API--wN8IK3wqINdycqcAr2UWemb6lLIJWYCRROZMq-P7mik", "url": "http://127.0.0.1:3000/", "httpOnly": true, "secure": false, "sameSite": "Lax"}, {"name": "__session", "value": "API--wN8IK3wqINdycqcAr2UWemb6lLIJWYCRROZMq-P7mik", "url": "http://localhost:3000/", "httpOnly": true, "secure": false, "sameSite": "Lax"}]);
   },
 };
